b080cad9b214e0e747630b341e4d1cbfb79f3d14
friendly-skill-4649 <jeffrey.palmowski@hotmail.com>
2020-07-22 00:18:40 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -20,9 +20,7 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
   sayMy();
-
   function sayMy() {
     console.log(name);
     var name = "Kristin";
